07d4fbc5030a84c49eb54649823784c7f47896d1
Bruno Alves <nito.dev@gmail.com>
2024-02-15 16:00:13 -0300

    feat: implements vote on poll controller

--- internal/infra/http/controllers/vote_on_poll_controller.go
+++ internal/infra/http/controllers/vote_on_poll_controller.go
@@ -12,8 +12,7 @@ type VoteOnPollController struct {
 }
 
 type VoteOnPollRequest struct {
-	Title   string   `json:"title" binding:"required,min=5,max=100"`
-	Options []string `json:"options" binding:"required,min=2"`
+	OptionId string `json:"option_id" binding:"required"`
 }
 
 // GetJWT godoc
@@ -26,7 +25,7 @@ type VoteOnPollRequest struct {
 // @Success      201
 // @Failure      400  {object} Error
 // @Failure      500  {object} Error
-// @Router       /polls [post]
+// @Router       /polls/{pollId}/vote [post]
 // @Security ApiKeyAuth
 func (ct *VoteOnPollController) Handle(c *gin.Context) {
 	var reqBody VoteOnPollRequest
@@ -44,6 +43,28 @@ func (ct *VoteOnPollController) Handle(c *gin.Context) {
 	if userId == "" {
 		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
 			"message": "Unauthorized",
+		})
+		return
+	}
+
+	pollId := c.Param("pollId")
+
+	if pollId == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "Missing pollId",
+		})
+		return
+	}
+
+	err = ct.voteOnPollUseCase.Execute(&usecases.VoteOnPollUseCaseRequest{
+		PollId:       pollId,
+		VoterId:      userId,
+		PollOptionId: reqBody.OptionId,
+	})
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
 		})
 		return
 	}
